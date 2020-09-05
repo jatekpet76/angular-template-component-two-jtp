ca16257620fd2333d9d824fe8df63c5c3028781f
jatekpet76 <jatekpet76@gmail.com>
2020-09-05 08:33:13 +0200

    Planing SubHandler - Comments

--- src/app/sub-handler/sub-handler.component.ts
+++ src/app/sub-handler/sub-handler.component.ts
@@ -1,5 +1,31 @@
 import { Component, OnInit, Input, TemplateRef } from '@angular/core';
 
+export interface SubHandler {
+  
+}
+
+/*
+ * Parameters:
+ * @Input() items - Data Source
+ * @Input() itemTemplate - List Item Template
+ * @Input() subHandler - Callbacks with return
+ * 
+ * Primary Functions:
+ * - Data: Input
+ * - Events: onAdd, onRemove, onSelect, onFilter... - ???
+ * - Create New Item: Callback
+ * - Render List Item: Input TemplateRef
+ * - Render New Item Form: Input TemplateRef
+ * - Handler FormGroup/FormArray: Input and Logic
+ * 
+ * Secondary Functions:
+ * - DataSource: Input - Observable
+ * - Render Filter Form: Input TemplateRef
+ * - Filter: Callback ??? - Call SubHandlerComponent to filter items, or send filtered items?
+ * - Remove Confirm Dialog - ??? - maybe boolean Input or Callback
+ * - 
+ * 
+ */
 @Component({
   selector: 'sub-handler',
   templateUrl: './sub-handler.component.html',
@@ -7,13 +33,11 @@ import { Component, OnInit, Input, TemplateRef } from '@angular/core';
 })
 export class SubHandlerComponent implements OnInit {
   @Input() items: any[];
-
-  @Input()
-  itemTemplate: TemplateRef<any>;
+  @Input() subHandler: SubHandler;
+  @Input() itemTemplate: TemplateRef<any>;
 
   constructor(
-    
-  ) { }
+  ) {}
 
   ngOnInit() {
   }
