d5edc96dfa226c0fbefcfe26bdcd3215b7739fd9
jatekpet76 <jatekpet76@gmail.com>
2020-09-05 08:07:08 +0200

    Now working - maybe the template cache stalled, have to add some captions and now working in SB.

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -2,18 +2,7 @@ import { Component, OnInit, Input, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'list',
-  template: `
-<ul>
-  <ng-container *ngFor="let item of items">
-    <li>
-      <ng-container 
-        [ngTemplateOutlet]="itemTemplate"
-        [ngTemplateOutletContext]="{item: item}"
-      ></ng-container>
-    </li>
-  </ng-container>
-</ul>
-  `
+  templateUrl: './list.component.html'
 })
 export class ListComponent implements OnInit {
   @Input() items: any[];
